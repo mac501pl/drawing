1156b50fe8b116053b80381bac8eb7ea960b697e
mac501pl <mzylka123@gmail.com>
2023-05-13 20:25:33 +0200

    basics done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  export let data;
+</script>
+
+<form method="POST">
+  {#if data.alreadyAPlayer}
+    <h1>Cześć, {data.playerData?.name}!</h1>
+    <input name="name" type="hidden" value={data.playerData?.name} />
+    ilu graczy? <input name="numberOfPlayers" type="number" />
+  {:else}
+    <h1>Cześć!</h1>
+    jak sie nazywasz?<input name="name" type="text" />
+    ilu graczy? <input name="numberOfPlayers" type="number" />
+  {/if}
+  <button>Rozpocznij nowa grę</button>
+</form>
